d8403a32f699a34a957b728375d8f4d14792f0f2
nhattran1206 <s3924826@rmit.edu.vn>
2023-12-11 18:13:53 +0700

    BSG-11 Fix Bug PIL.UnidentifiedImageError

--- frontend/main.py
+++ frontend/main.py
@@ -1,10 +1,13 @@
 import requests
 import streamlit as st
 import os
-from PIL import Image
+import imghdr
+import shutil
+import tempfile
 import pymongo
 from io import BytesIO
 from dotenv import dotenv_values, load_dotenv
+
 
 load_dotenv()
 
@@ -45,10 +48,16 @@ def show_upload_archive():
     if uploaded_files is not None:
         for uploaded_file in uploaded_files:
             try:
-                image = Image.open(uploaded_file)
-                st.image(image, caption="Uploaded image", use_column_width=True)
+                # Create a temporary folder
+                temp_folder = tempfile.mkdtemp()
 
-                image_details = get_image_details(image)
+                # Save the content of the uploaded file to a temporary file
+                temp_image_path = os.path.join(temp_folder, "temp_image.png")
+                with open(temp_image_path, "wb") as temp_file:
+                    temp_file.write(uploaded_file.read())
+                st.image(uploaded_file, caption="Uploaded image", use_column_width=True)
+
+                image_details = get_image_details(temp_image_path)
                 st.write("Image details:")
                 st.write(image_details)
 
@@ -72,37 +81,17 @@ def show_upload_archive():
                     st.error("Failed to receive a valid response.")
 
                 if st.button("Archive image", key=idx):
-                    archive_image_mongodb(image, image_details, result=result.get("species"))
+                    archive_image_mongodb(temp_image_path, image_details, result=result.get("species"))
                     st.success("Image archived successfully!")
 
                 st.success("File uploaded successfully!")
 
             except Exception as e:
                 st.error(f"Error processing image: {str(e)}")
+            finally:
+                # Delete the temporary folder and its contents
+                shutil.rmtree(temp_folder, ignore_errors=True)
             idx += 1
-
- 
-# uploaded_file = st.file_uploader("Upload an image file", type=["jpg", "jpeg", "png"])
-# if uploaded_file is not None:
-#     st.write("File uploaded successfully!")
-#     files = {"image": uploaded_file}
-
-#     response = requests.post(BACKEND_URI, files=files)
-        
-#     if response.status_code == 200:
-#         result = response.json()
-#         st.write("Species:", result["species"])
-#         # Assuming result["recommendations"] is a list of image paths
-#         image_paths = result["recommendations"]
-
-#         # Prepend the server URL to the image paths
-#         server_url = "http://localhost:8000/images/"
-#         full_image_urls = [server_url + path for path in image_paths]
-
-#         # Display the images
-#         st.image(full_image_urls)
-#     else:
-#         st.error("Failed to receive a valid response.")
 
 
 def show_view_archive():
@@ -110,19 +99,20 @@ def show_view_archive():
 
     archived_images = collection.find({})
     for archived_image in archived_images:
-        st.image(BytesIO(archived_image["image"]), caption=f"Archived Image - {archived_image['width']}x{archived_image['height']}", use_column_width=True)
+        st.image(BytesIO(archived_image["image"]), caption=f"Size - {archived_image['size in mb']} MB", use_column_width=True)
         st.write(archived_image["result"])
-        if st.button(f"Delete Image - {archived_image['width']}x{archived_image['height']}",
+        if st.button(f"Delete Image - {archived_image['size in mb']} MB",
                     key=archived_image['_id']):
             delete_image_mongodb(archived_image['_id'])
             st.success("Image deleted successfully!")
             
 
-def archive_image_mongodb(image, image_details, result=None):
-    image_bytes = image_to_bytes(image)
+def archive_image_mongodb(temp_image_path, image_details, result=None):
+    with open(temp_image_path, "rb") as image_file:
+        image_bytes = image_file.read()
+    # image_bytes = image_to_bytes(image)
     document = {
-        "width": image_details["Width"],
-        "height": image_details["Height"],
+        "size in mb": image_details["File Size in mb"],
         "format": image_details["Format"],
         "image": image_bytes,
         "result": result,
@@ -130,21 +120,25 @@ def archive_image_mongodb(image, image_details, result=None):
     collection.insert_one(document)
 
 
-def image_to_bytes(image):
-    img_byte_arr = BytesIO()
-    image.save(img_byte_arr, format=image.format)
-    return img_byte_arr.getvalue()
-
-
-def get_image_details(image):
-    width, height = image.size
-    format_type = image.format
+def get_image_details(image_path):
+    size = os.path.getsize(image_path)  # Get file size in bytes
+    mb_size = str_to_mb(size)
+    image_format = imghdr.what(image_path)  # Get image format using imghdr
     details = {
-        "Width": width,
-        "Height": height,
-        "Format": format_type,
+        "File Size in mb": mb_size,
+        "Format": image_format,
+        # Add more details as needed
     }
     return details
+
+
+def str_to_mb(string_size):
+    bytes_size = int(string_size)
+    return bytes_to_mb(bytes_size)
+
+
+def bytes_to_mb(bytes_size):
+    return round(bytes_size / (1024 * 1024), 2)
 
 
 def delete_image_mongodb(image_id):
